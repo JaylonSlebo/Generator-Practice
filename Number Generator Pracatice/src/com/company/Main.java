c196340cf2b393438ba5f413d1c70f84f85ffbc8
JaylonSlebo <60892085+JaylonSlebo@users.noreply.github.com>
2020-04-01 14:54:34 -0400

    Add files via upload

---
+++ Number Generator Pracatice/src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code her
+
+        System.out.println((int) (Math.random() * 100));
+        System.out.println((int) (Math.random() * 100));
+        System.out.println((int) (Math.random() * 100));
+        System.out.println((int) (Math.random() * 100));
+        System.out.println((int) (Math.random() * 100));
+        System.out.println((int) (Math.random() * 100));
+        System.out.println((int) (Math.random() * 100));
+        }
+
+
+
+
+
+
+
+}
